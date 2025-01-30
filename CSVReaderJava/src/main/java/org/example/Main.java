12502c63abe760fa9df8e742f58377e534cb5142
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-01-30 17:47:19 -0500

    done coding feature 3 - adding records to the csv file

--- CSVReaderJava/src/main/java/org/example/Main.java
+++ CSVReaderJava/src/main/java/org/example/Main.java
@@ -10,6 +10,13 @@ public class Main {
     public static void main(String[] args) {
         ExpenseService expenseService = new ExpenseService();
 
+        expenseService.addExpenseRecord(
+                LocalDate.of(2025, 1, 30),
+                "Office Supplies",
+                "Printer paper",
+                new BigDecimal("8.99"),
+                "Company Card"
+        );
         ConsoleUI ui = new ConsoleUI(expenseService);
 
         ui.run();
